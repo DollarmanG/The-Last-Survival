b6a8829c6f6e124f0beabb14c76bf0eaf1afd5cc
DollarmanG <noman.syed@yh.nackademin.se>
2022-11-21 11:22:47 +0100

    adjusted code
    adjusted code

--- Assets/Scripts/Wolf.cs
+++ Assets/Scripts/Wolf.cs
@@ -8,6 +8,11 @@ public class Wolf : MonoBehaviour
     
     private AudioSource wolfSound;
 
+    void Start()
+    {
+        wolfSound = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
